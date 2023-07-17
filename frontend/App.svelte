077b8d0f28db9abe0f5fb71235afd494859d60fd
clomia <clomia.sig@gmail.com>
2023-07-18 08:42:46 +0900

    간단한 라우팅

--- frontend/App.svelte
+++ frontend/App.svelte
@@ -1,53 +1,20 @@
-<script lang="ts">
-	// Echarts
-	import * as echarts from "echarts";
+<script>
+	import { Router, Link, Route } from "svelte-routing";
+	import Home from "./routes/Home.svelte";
+	import SignIn from "./routes/SignIn.svelte";
+	import Head from "./components/Head.svelte";
 
-	const host = window.location.origin;
-
-	let chartBox: HTMLElement | undefined;
-	let searchText = "";
-
-	async function getData(symbol: string) {
-		const url = `${host}/api/data/time-series/symbol/adj-close?element=${symbol}`;
-		const res = await fetch(url);
-		return await res.json();
-	}
-
-	async function search() {
-		console.log("요청 발신!");
-		const data = await getData(searchText);
-		console.log("응답 수신!");
-		const chart = echarts.init(chartBox);
-		const option = {
-			xAxis: {
-				type: "category",
-				data: data.t,
-			},
-			yAxis: {
-				type: "value",
-			},
-			series: [
-				{
-					data: data.values,
-					type: "line",
-				},
-			],
-		};
-		chart.setOption(option);
-	}
+	export let url = "/";
 </script>
 
-<main>
-	<input type="text" bind:value={searchText} />
-	<button on:click={search}>Search</button>
-	<div id="chart" bind:this={chartBox} style="width: 600px;height:400px;" />
-</main>
-
-<style>
-	main {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: space-evenly;
-	}
-</style>
+<Head />
+<Router {url}>
+	<nav>
+		<Link to="/">Home</Link>
+		<Link to="/sign-in">SignIn</Link>
+	</nav>
+	<div>
+		<Route path="/sign-in" component={SignIn} />
+		<Route path="/" component={Home} />
+	</div>
+</Router>

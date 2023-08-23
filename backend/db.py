92232e34ada4fd3f07f8645ba00b66e04f61b1b4
clomia <clomia.sig@gmail.com>
2023-08-23 22:01:35 +0900

    Secrets Manager 암호 교체 동작 반영

--- backend/db.py
+++ backend/db.py
@@ -1,3 +1,6 @@
+import json
+
+import boto3
 import psycopg
 
 from backend.system import SECRETS, log
@@ -17,9 +20,19 @@ def execute_query(query: str) -> list:
         return cur.fetchall()  # case: read success
     except psycopg.ProgrammingError as e:
         return []  # case: write success
+    except psycopg.OperationalError as e:  # password authentication failed
+        # Secrets Manager에서 암호 교체가 이루어졌다고 간주하고 암호 업데이트 후 재시도
+        latest_password = json.loads(
+            boto3.client("secretsmanager").get_secret_value(
+                SecretId=SECRETS["RDS_SECRET_MANAGER_ARN"]
+            )["SecretString"]
+        )["password"]
+        SECRETS["DB_PASSWORD"] = latest_password
+        log.warning(f"[{e}] DB 연결 오류가 발생하였습니다. Secrets Manager로부터 암호를 업데이트하여 재시도합니다.")
+        return execute_query(query)
     except Exception as e:
         conn.rollback()
-        log.critical(f"DB 쿼리 실행 오류가 발생하여 롤백하였습니다. {e}\n{query}")
+        log.critical(f"[{e}] DB 쿼리 실행 오류가 발생하여 롤백하였습니다.\nQuery: {query}")
         raise e
     finally:
         cur.close()

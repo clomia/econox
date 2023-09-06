58ece98bb87c6237673c5c535c4f3c09bcf47cb5
clomia <clomia.sig@gmail.com>
2023-09-06 15:02:01 +0900

    리디렉션 로직 제거, 클라우드에서 하도록 변경

--- frontend/App.svelte
+++ frontend/App.svelte
@@ -10,10 +10,6 @@
     onMount(async () => {
         const { text } = await loadUiText();
         state.uiText.text.set(text);
-        if (window.location.hostname === "econox.io") {
-            window.location.href = // 서브도메인(www.)이 없으면 붙여줍니다.
-                "https://www.econox.io" + window.location.pathname + window.location.search;
-        }
     });
 </script>
 

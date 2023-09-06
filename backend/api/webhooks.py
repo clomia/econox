867229c9d4515ca2a9a5f71022c3ce164c79ed46
clomia <clomia.sig@gmail.com>
2023-09-06 11:12:52 +0900

    PayPal 웹훅 내용 확인

--- backend/api/webhooks.py
+++ backend/api/webhooks.py
@@ -1,3 +1,19 @@
+from fastapi import Request
+
 from backend.http import APIRouter
 
 router = APIRouter("webhooks")
+
+
+@router.public.post("/paypal")
+async def paypal_webhook(request: Request):
+    """
+    - 으앙
+    - Response: BillingKey
+    """
+    payload = await request.json()
+    print(request)
+    print(request.headers)
+    print(payload)
+    payload["event_type"]
+    return {}

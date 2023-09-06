737d34f3d04ee1e86a2a8db8fe61d5fce59e74da
clomia <clomia.sig@gmail.com>
2023-09-06 13:45:24 +0900

    indent space 오류 해결

--- frontend/App.svelte
+++ frontend/App.svelte
@@ -1,36 +1,36 @@
 <script>
-	import { onMount } from "svelte";
-	import { Router, Route } from "svelte-routing";
-	import { routes } from "./pages";
-	import Navigator from "./components/Navigator.svelte";
-	import LangBtn from "./components/LangBtn.svelte";
-	import { loadUiText } from "./modules/uiText";
-	import * as state from "./modules/state";
+    import { onMount } from "svelte";
+    import { Router, Route } from "svelte-routing";
+    import { routes } from "./pages";
+    import Navigator from "./components/Navigator.svelte";
+    import LangBtn from "./components/LangBtn.svelte";
+    import { loadUiText } from "./modules/uiText";
+    import * as state from "./modules/state";
 
-	onMount(async () => {
-		const { text } = await loadUiText();
-		state.uiText.text.set(text);
-		if (window.location.hostname === "econox.io") {
-			window.location.href =
-				"https://www.econox.io" + window.location.pathname + window.location.search;
-		}
-	});
+    onMount(async () => {
+        const { text } = await loadUiText();
+        state.uiText.text.set(text);
+        if (window.location.hostname === "econox.io") {
+            window.location.href =
+                "https://www.econox.io" + window.location.pathname + window.location.search;
+        }
+    });
 </script>
 
 <header>
-	<Navigator />
+    <Navigator />
 </header>
 
 <main>
-	<Router>
-		{#each routes as route}
-			<Route path={route.path}>
-				<svelte:component this={route.page} />
-			</Route>
-		{/each}
-	</Router>
+    <Router>
+        {#each routes as route}
+            <Route path={route.path}>
+                <svelte:component this={route.page} />
+            </Route>
+        {/each}
+    </Router>
 </main>
 
 <aside>
-	<LangBtn />
+    <LangBtn />
 </aside>

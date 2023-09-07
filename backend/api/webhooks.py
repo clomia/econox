3b08094473ea152174a42936b00cd28937ed0da4
clomia <clomia.sig@gmail.com>
2023-09-07 21:13:19 +0900

    Paypal verify_webhooks test

--- backend/api/webhooks.py
+++ backend/api/webhooks.py
@@ -1,4 +1,4 @@
-from fastapi import Request
+from fastapi import Request, Depends
 
 from backend.http import APIRouter, PayPalAPI
 from backend.system import SECRETS
@@ -6,7 +6,7 @@ from backend.system import SECRETS
 router = APIRouter("webhooks")
 
 
-@router.public.post("/paypal")
+@router.public.post("/paypal", dependencies=[Depends({PayPalAPI.verify_webhook})])
 async def paypal_webhook(request: Request):
     """
     - 으앙
@@ -16,15 +16,4 @@ async def paypal_webhook(request: Request):
     print(request)
     print(request.headers)
     print(payload)
-    await PayPalAPI("/v1/notifications/verify-webhook-signature").post(
-        {
-            "auth_algo": request.headers["paypal-auth-algo"],
-            "cert_url": request.headers["paypal-cert-url"],
-            "transmission_id": request.headers["paypal-transmission-id"],
-            "transmission_sig": request.headers["paypal-transmission-sig"],
-            "transmission_time": request.headers["paypal-transmission-time"],
-            "webhook_id": SECRETS["PAYPAL_WEBHOOK_ID"],
-            "webhook_event": payload,
-        }
-    )
     return {}

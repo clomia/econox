5d37672e46eb2131044c435c807e175da738143f
clomia <clomia.sig@gmail.com>
2023-11-28 12:59:51 +0900

    data에 디버깅을 위한 로깅 추가

--- backend/api/data.py
+++ backend/api/data.py
@@ -6,6 +6,7 @@ from aiocache import cached
 from backend.http import APIRouter
 from backend.math import datetime2utcstr
 from backend.data import fmp, world_bank
+from backend.system import log
 
 router = APIRouter("data")
 
@@ -22,20 +23,28 @@ async def search_symbols_and_countries(query: str, lang: str):
     query = query.strip()
 
     async def parsing(obj):
+        log.info(f"[시작][/elements] name, note 가져오기 -> {obj}")
         name, note = await asyncio.gather(obj.name.en(), obj.note.trans(to=lang))
+        log.info(f"[완료][/elements] name, note 가져오기 -> {obj} -> {name}, {note}")
         return {
             "code": obj.code,
             "name": name,
             "note": note,
         }
 
+    log.info(f"[시작] {query} symbol들과 country들을 fmp.search, world_bank.search를 통해 검색")
     symbol_objects, country_objects = await asyncio.gather(
         fmp.search(query), world_bank.search(query)
+    )
+    log.info(
+        f"[완료] {query} symbol들과 country들을 fmp.search, world_bank.search를 통해 검색 -> {symbol_objects}, {country_objects}"
+        f"[시작] 검색된 오브젝트들에서 JSON 파싱 가능한 객체로 파싱"
     )
     symbols, countries = await asyncio.gather(
         asyncio.gather(*[parsing(sym) for sym in symbol_objects]),
         asyncio.gather(*[parsing(ctry) for ctry in country_objects]),
     )
+    log.info(f"[완료] {query} symbols={symbols}  countries={countries}")
     return {
         "symbols": symbols,
         "countries": countries,
@@ -50,9 +59,11 @@ async def search_news_related_to_symbols(symbol: str, lang: str):
     """
 
     async def parsing(obj):
+        log.info(f"[시작][/news] title, content 가져오기 -> {obj}")
         title, content = await asyncio.gather(
             obj.title.trans(to=lang), obj.content.trans(to=lang)
         )
+        log.info(f"[완료][/news] title, content 가져오기 -> {obj} -> {title}, {content}")
         return {
             "title": title,
             "content": content,
@@ -60,12 +71,20 @@ async def search_news_related_to_symbols(symbol: str, lang: str):
             "date": datetime2utcstr(obj.date),
         }
 
+    log.info(f"[시작] {symbol} 객체 load, 뉴스 검색")
     symbol_obj, news_objects = await asyncio.gather(
         fmp.Symbol(code=symbol).load(), fmp.news(symbol)
+    )
+    log.info(
+        f"[완료] {symbol} 객체 load, 뉴스 검색 -> {symbol_obj, news_objects}"
+        f"[시작] {symbol} 객체 name, note 가져오기, 뉴스들 title, content 가져오기"
     )
     symbol_name, symbol_note, *news = await asyncio.gather(
         *[symbol_obj.name.en(), symbol_obj.note.trans(to=lang)]
         + [parsing(news_obj) for news_obj in news_objects]
+    )
+    log.info(
+        f"[완료] {symbol} 객체 name, note 가져오기, 뉴스들 title, content 가져오기 -> {symbol_name}, {symbol_note}, {news}"
     )
     return {
         "contents": list(reversed(news)),  # 최신 -> 과거 순으로 정렬

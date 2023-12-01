78d910faafed1d32a2eb11ed613f3b48b90bf9d4
clomia <clomia.sig@gmail.com>
2023-12-01 15:16:18 +0900

    db 모듈 리펙토링 완료

--- backend/db.py
+++ backend/db.py
@@ -1,17 +1,36 @@
 """
-- exec 함수로 자유롭게 쿼리를 실행하세요
-- 복잡한 쿼리는 Template 클래스를 통해 간결하게 생성하세요
-- Transaction 클래스를 사용하면 단일 트렌젝션으로 여러개의 쿼리를 원하는 순서로 실행할 수 있습니다.
-    - 여러 쿼리 실행은 exec 함수로도 가능하나, Template을 쓰는 경우 Transaction 클래스를 사용해야 합니다.
-- 자주 사용되는 쿼리 로직은 간단한 함수로 만들어 사용하세요 (예시: user_exists, signup_history_exists)
-- 쿼리 작성 시 세미콜론은 작성하지 않는걸 기본 컨벤션으로 합니다, 세미콜론 처리는 이 모듈에 맡기세요
-    - [주의!] 단, db.exec에 문자열로 여러 쿼리를 넣는 경우는 세미콜론을 넣어줘야 합니다. 
+```
+# 1. 단일 쿼리 실행하기
+sql = db.SQL("SELECT * FROM elements WHERE code={code}", params={"code": "AAPLE"})
+element = await sql.exec()
+print(element) # {'id': 8, 'section': 'country', 'code': 'AAPLE'}
+
+# 2. 여러 SQL을 하나의 트렌젝션으로 실행하기
+read_sql = db.SQL("SELECT * FROM elements WHERE code={code}", params={"code": "AAPLE"})
+write_sql = db.SQL(
+    "INSERT INTO elements (section, code) VALUES ({section}, {code})",
+    params={"section": "country", "code": "USA"},
+    fetch=False,
+)
+fetched = await db.exec(read_sql, write_sql)
+element = fetched[read_sql]
+print(element) # {'id': 8, 'section': 'country', 'code': 'AAPLE'}
+```
+- SQL 클래스를 사용해서 단일 쿼리를 정의하세요.
+- exec 함수를 사용해서 쿼리를 실행하세요.
+    - SQL 객체의 exec 메서드를 통해 단일 쿼리를 실행할 수 있습니다.
+    - 모듈에 정의된 exec 함수를 통해 여러 쿼리를 단일 트렌젝션으로 실행할 수 있습니다.
+- psycopg 클라이언트 예외 처리 
+    - psycopg 에러를 통해 DB의 응답을 구분하세요
+    - __cause__ 속성을 통해 QueryError 객체에 접근할 수 있습니다.
+    - QueryError의 sql 속성을 통해 실행된 SQL 객체에 접근할 수 있습니다.
 """
+from __future__ import annotations
 
 import re
 import json
 import asyncio
-from typing import List, Tuple, Dict, Any
+from typing import List, Tuple, Dict, Any, Literal
 
 import boto3
 import psycopg
@@ -22,32 +41,10 @@ from backend.system import SECRETS, log
 # ======== 쿼리 실행 함수 =========
 
 
-class SQL:
-    def __init__(self, query: str, params: Dict[str, Any] = {}, fetch: bool = True):
-        """PostgreSQL 문자열 컨벤션이 아닌 python 객체를 사용합니다. 문자열을 ''로 감싸지 마세요"""
-        if "SELECT" not in query.upper() and fetch is True:
-            raise Exception(f"[SQL 객체 생성 불가] Write 쿼리는 fetch를 수행할 수 없습니다. ({query})")
-        self.query = query
-        self.params = params
-        self.fetch = fetch
-
-    def __repr__(self) -> str:
-        return f"<SQL (fetch={self.fetch}) {self.query} >"
-
-    def encode(self) -> Tuple[str, tuple]:
-        # 쿼리에서 사용되는 파라미터 키들
-        param_keys = re.findall(r"\{(.*?)\}", self.query)
-        # 쿼리에 넣어줘야 하는 파라미터 값들
-        param_values = tuple(self.params[key] for key in param_keys)
-        # 쿼리의 {}를 %s로 변환
-        query_converted = re.sub(r"\{(.*?)\}", "%s", self.query)
-        return query_converted, param_values
-
-
 class QueryError(Exception):
-    def __init__(self, sql: SQL):
+    def __init__(self, sql: SQL, msg: str = ""):
         self.sql = sql
-        super().__init__(f"SQL 실행 실패: {sql}")
+        super().__init__(f"[DB Error] {sql} {msg}")
 
 
 async def exec(
@@ -56,20 +53,25 @@ async def exec(
     _retry=False,
 ) -> Dict[SQL, None | List[Dict[str, Any]]]:
     """
-    - psycopg3의 비동기 클라이언트를 사용해서 여러 쿼리를 동시에 실행합니다.
-        - 너무 많은 쿼리를 동시에 실행하면 DB 응답이 늦을 수 있습니다.
-        - 실제로 쿼리의 병렬 처리 능력은 AWS RDS 클러스터의 인스턴스 갯수에 따라 다릅니다.
+    - 여러 SQL을 동시에 실행합니다. 하나의 트렌젝션으로 취급되며 하나라도 실패하면 모두 안전하게 롤백됩니다.
     - retuen: SQL에 대한 결과가 딕셔너리로 매핑되어 반환됩니다.
         - 그리고 결과는 컬럼과 값이 매핑된 딕셔너리입니다.
     - exception: 발생된 예외의 __cause__ 속성을 통해 QueryError 인스턴스를 가져올 수 있습니다.
         - 그리고 QueryError 객체의 sql 속성을 통해 실패한 SQL 객체를 가져올 수 있습니다.
+    - _retry: 함수 내부적으로 재시도 로직에 사용되므로 외부에서 값을 주입하지 마세요.
     """
 
-    async def execute_query(_sql: SQL, cur):
+    async def _exec(_sql: SQL, cur):
         query, params = _sql.encode()
         try:
             await cur.execute(query, params)
-            return await cur.fetchall() if _sql.fetch else None
+            if _sql.fetch is False:
+                return None
+            rows = await cur.fetchall()
+            if _sql.fetch == "all":
+                return rows
+            elif _sql.fetch == "one":
+                return rows[0] if rows else None
         except Exception as e:
             # DB 에러에 따른 분기 처리를 위해서 psycopg의 예외 클래스를 raise 해야 함
             raise e from QueryError(_sql)  # e.__cause__ = QueryError(_sql)
@@ -85,7 +87,7 @@ async def exec(
             if _retry:
                 log.info("[DB] Secrets Manager로부터 업데이트된 암호로 인증에 성공하였습니다.")
             async with conn.cursor() as cur:
-                tasks = [execute_query(_sql, cur) for _sql in sql]
+                tasks = [_exec(_sql, cur) for _sql in sql]
                 results = await asyncio.gather(*tasks)
                 return {_sql: result for _sql, result in zip(sql, results)}
     except psycopg.OperationalError as e:
@@ -101,69 +103,79 @@ async def exec(
         return exec(*sql, dbname, _retry=True)  # 재시도
 
 
-class Transaction:
-    def __init__(self, dbname: str = "econox"):
+class SQL:
+    def __init__(
+        self,
+        query: str,
+        params: Dict[str, Any] = {},
+        fetch: Literal[False, "one", "all"] = "one",
+    ):
         """
-        - 단일 트렌젝션으로 여러개의 쿼리를 실행합니다
-        - prepend, append 메서드를 통해 쿼리 순서를 설정하세요
-            - prepend(앞에 추가), append(뒤에추가)
-        - set 메서드를 사용해서 한번에 여러개를 추가할 수 있습니다.
+        - PostgreSQL 문자열 컨벤션이 아닌 python 객체를 사용합니다. 문자열을 ''로 감싸지 마세요
+        - 쿼리 마지막에 세미콜론 붙일 필요 없습니다.
+        - params의 키는 숫자일 수 없습니다. 문자열만 허용됩니다.
+        - fetch:
+            - False: 쓰기 쿼리인 경우 -> None
+            - "one": 하나의 레코드만 읽는 경우 -> Dict[str, Any]
+                - 응답된 레코드가 하나도 없는 경우 -> None
+            - "all": 여러 레코드를 읽는 경우 -> List[Dict[str, Any]]
+                - 응답된 레코드가 하나도 없는 경우 -> []
         """
-        self.dbname = dbname
-        self.query_list = []
-        self.param_dict = {}
-
-    def prepend(self, query: str, params):
-        if (query := query.strip()) and (query[-1] != ";"):
-            query += ";"  # 여러 쿼리 넣을땐 ; 로 구분해줘야 함
-        self.query_list.insert(0, query)
-        self.param_dict = params | self.param_dict
-
-    def append(self, query: str, **params):
         if (query := query.strip()) and (query[-1] != ";"):
             query += ";"
-        self.query_list.append(query)
-        self.param_dict |= params
+        self.query = query
+        self.params = params
+        self.fetch = fetch
+        # QueryError 쓰려면 우선 SQL 객체가 구성되어야 하므로 이 코드들은 밑에 있어야 함
+        if query.upper()[:6] != "SELECT" and fetch is not False:
+            error_msg = f"[SQL 객체 생성 불가] Write 쿼리는 fetch를 수행할 수 없습니다. ({query})"
+            raise QueryError(self, error_msg)
+        if fetch not in [False, "one", "all"]:
+            error_msg = f"[SQL 객체 생성 불가] fetch 매개변수는 {fetch} 일 수 없습니다."
+            raise QueryError(self, error_msg)
 
-    async def exec(self):  # 하나의 SQL 객체로 만들어서 exec 해야 함!
-        query = SQL(" ".join(self.query_list), params=self.param_dict)
-        return await exec(query, dbname=self.dbname)
+    def __repr__(self) -> str:
+        repr_query = self.query.replace("\n", "  ")
+        return f"<SQL (fetch={self.fetch}) {repr_query.format(**self.params)} >"
+
+    def encode(self) -> Tuple[str, tuple]:
+        # 쿼리에서 사용되는 파라미터 키들
+        param_keys = re.findall(r"\{(.*?)\}", self.query)
+        # 쿼리에 넣어줘야 하는 파라미터 값들
+        param_values = tuple(self.params[key] for key in param_keys)
+        # 쿼리의 {}를 %s로 변환
+        query = re.sub(r"\{(.*?)\}", "%s", self.query)
+        return query, param_values
+
+    async def exec(self, dbname: str = "econox"):
+        fetched = await exec(self, dbname=dbname)
+        return fetched[self]
 
 
-async def select_row(table: str, fields: list, where: dict):
-    """
-    - limit 1 로 하나의 레코드만 선택하여 dict 형태로 반환합니다.
-    - 레코드가 없으면 빈 딕셔너리 반환
-    """
-    values = await exec(
-        template=Template(table=table).select_query(*fields, where=where, limit=1),
-        embed=True,
-    )
-    return {k: v for k, v in zip(fields, values)}
+# ======================== 단축 함수들 ========================
 
 
 async def user_exists(email: str) -> bool:
-    query = "SELECT 1 FROM users WHERE email={email};"
-    return bool(await exec(query, params={"email": email}))
+    sql = SQL("SELECT id FROM users WHERE email={email}", params={"email": email})
+    return bool(await sql.exec())
 
 
 async def signup_history_exists(email: str, phone: str) -> bool:
     query = """
-        SELECT 1 FROM signup_histories 
+        SELECT id FROM signup_histories 
         WHERE email={email} or phone={phone}
-        LIMIT 1;
-    """
-    return bool(await exec(query, params={"email": email, "phone": phone}))
+        LIMIT 1
+    """  # email, phone이 UNIQUE가 아니라서 LIMIT 1 해줘야 함
+    sql = SQL(query, params={"email": email, "phone": phone})
+    return bool(await sql.exec())
 
 
 async def payment_method_exists(email: str) -> bool:
     """결제수단 등록 여부"""
-    db_user = await select_row(
-        "users",
-        fields=["currency", "tosspayments_billing_key", "paypal_subscription_id"],
-        where={"email": email},
-    )
+    sql = SQL("SELECT * FROM users WHERE email={email}", {"email": email})
+    if user := await sql.exec() is None:
+        raise QueryError(sql, "[payment_method_exists]: 해당하는 유저가 없습니다!")
     return bool(
-        (db_user["currency"] == "KRW" and db_user["tosspayments_billing_key"])
-        or (db_user["currency"] == "USD" and db_user["paypal_subscription_id"])
+        (user["currency"] == "KRW" and user["tosspayments_billing_key"])
+        or (user["currency"] == "USD" and user["paypal_subscription_id"])
     )

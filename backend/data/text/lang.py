27c063c6b29bf6d9e96dc3eb89e907d5724b5dab
clomia <clomia.sig@gmail.com>
2023-12-14 20:58:48 +0900

    동기 클라이언트로 변경

--- backend/data/text/lang.py
+++ backend/data/text/lang.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 
 import deepl
 import httpx
-import redis.asyncio as redis
+import redis  # .asyncio as redis
 
 from backend.system import SECRETS
 from backend.http import pooling
@@ -24,15 +24,12 @@ for name, glossary in glossaries_json.items():
     for from_txt, to_txt in glossary.items():
         glossaries[to_lang][from_txt] = to_txt
 
-radis_host = SECRETS["RADIS_HOST"]
-print(f"radis_host: {radis_host}")
-
 
 class DeeplCache:
     """Redis를 백엔드로 사용합니다"""
 
     expire = 360 * 24 * 30
-    cache = redis.Redis(host=radis_host, decode_responses=True)
+    cache = redis.Redis(host=SECRETS["RADIS_HOST"], decode_responses=True)
 
     def __init__(self, to_lang: str, from_lang: str = None):
         self.key_prefix = f"{str(from_lang).lower()}-{to_lang.lower()}"
@@ -43,12 +40,12 @@ class DeeplCache:
     async def set(self, key: str, value: str):
         _key = self.cache_key(key)
         print(f"[시작] SET key: {key}")
-        await self.cache.set(_key, str(value), ex=self.expire)
+        self.cache.set(_key, str(value), ex=self.expire)
         print(f"[종료] SET key: {key}")
 
     async def get(self, key: str):
         print(f"[시작] GET key: {key}")
-        value = await self.cache.get(self.cache_key(key))
+        value = self.cache.get(self.cache_key(key))
         print(f"[종료] GET key: {key}")
         return value
 

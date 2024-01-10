db3b98878457e73cb18011066948fd94a028b1c0
clomia <clomia.sig@gmail.com>
2024-01-10 21:52:56 +0900

    읽기 작업에 대해 메타데이터 확인 로직 추가

--- backend/data/io.py
+++ backend/data/io.py
@@ -26,7 +26,14 @@ def xr_open_zarr(path: Path):
         - EFS에 .zarr 폴더가 있고, 내부 데이터가 완성되기 전에 읽으면 FileNotFoundError가 발생한다.
             네트워크 속도 문제도 있겠지만, .zarr 폴더 자체가 없다는 에러도 발생했기 때문에 복잡한 안정성 문제로 취급.
     """
-    return _pooling(partial(xr.open_zarr, path))
+
+    def proxy():
+        data = xr.open_zarr(path)
+        # 메타데이터 잘 불러와졌는지 체크
+        assert data.attrs.get("client", {}).get("collected")
+        return data
+
+    return _pooling(proxy)
 
 
 def xr_to_zarr(dataset: xr.Dataset, path: Path):

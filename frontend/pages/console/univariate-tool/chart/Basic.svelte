af60a91d802b5a723aa9d55527732403b3af1cd7
clomia <clomia.sig@gmail.com>
2024-01-10 17:27:23 +0900

    기초적인 시각화 시험

--- frontend/pages/console/univariate-tool/chart/Basic.svelte
+++ frontend/pages/console/univariate-tool/chart/Basic.svelte
@@ -5,11 +5,25 @@
 
   export let chartSource: SourceType;
 
+  const option: echarts.EChartsOption = {
+    dataset: {
+      source: chartSource.original,
+    },
+    xAxis: { type: "time" },
+    yAxis: { splitLine: { show: false } },
+    series: [{ type: "scatter", symbolSize: 3, color: "white" }],
+    tooltip: {
+      formatter: (params: any) => {
+        return "X: " + params.value[0] + "<br>Y: " + params.value[1];
+      },
+    },
+  };
+
   let chartContainer: HTMLElement;
   let chart: echarts.ECharts;
   onMount(() => {
     chart = echarts.init(chartContainer);
-    console.log(chart, chartSource);
+    chart.setOption(option);
   });
 </script>
 
@@ -18,4 +32,8 @@
 </main>
 
 <style>
+  .chart {
+    width: 41rem;
+    height: 22rem;
+  }
 </style>

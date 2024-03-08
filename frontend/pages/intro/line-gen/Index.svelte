a09803e8fc114dc8d6a8a5822775b1ed92d00117
clomia <clomia.sig@gmail.com>
2024-03-08 16:09:09 +0900

    차트 무한생성

--- frontend/pages/intro/line-gen/Index.svelte
+++ frontend/pages/intro/line-gen/Index.svelte
@@ -16,36 +16,66 @@
   };
 
   const render = (chart: echarts.ECharts, dataSource: any[]) => {
+    const seriesSource = dataSource[0].slice(1);
     const option = {
-      animationDuration: 3000,
       dataset: [{ source: dataSource }],
       xAxis: {
         type: "time",
         show: false,
       },
       yAxis: { show: false },
-      series: dataSource[0].slice(1).map((feature: string) => {
+      series: seriesSource.map((feature: string) => {
         return {
+          animation: false,
+          silent: true,
           emphasis: { disabled: true },
           name: feature,
           type: "line",
           symbol: "none",
-          connectNulls: true,
           lineStyle: { color: colormap[feature], opacity: 1, width: 1 },
-          encode: {
-            x: "t",
-            y: feature,
-          },
+          encode: { x: "t", y: feature },
         };
       }),
     };
     chart.setOption(option);
   };
 
+  const cyclicWindow = (
+    arr: any[],
+    windowLength: number,
+    startIndex: number
+  ) => {
+    const n = arr.length;
+    startIndex = startIndex % n;
+    const result = [];
+    for (let i = 0; i < windowLength; i++) {
+      const currentIndex = (startIndex + i) % n;
+      result.push(arr[currentIndex]);
+    }
+    return result;
+  };
+  const replaceFirstColumn = (matrix: any[], newColumn: any[]) => {
+    for (let i = 0; i < matrix.length; i++) {
+      matrix[i][0] = newColumn[i];
+    }
+    return matrix;
+  };
+
   const init = () => {
     const chart = echarts.init(chartContainer);
     window.onresize = chart.resize;
-    render(chart, data);
+
+    const header = data.shift();
+    const windowSize = Math.floor(data.length / 2); // header제외
+    const defaultDate = cyclicWindow(data, windowSize, 0).map((row) => row[0]); // 고정 날짜정보 저장
+    let cnt = 0;
+    setInterval(() => {
+      const _window = cyclicWindow(data, windowSize, cnt);
+      const _arr = replaceFirstColumn(_window, defaultDate);
+      _arr.unshift(header);
+      render(chart, _arr);
+      cnt += 1;
+    }, 10);
   };
 
   onMount(init);

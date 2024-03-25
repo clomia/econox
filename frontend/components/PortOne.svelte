4e02c28c30f34817f77961e5a817d4a0eaa34efc
clomia <clomia.sig@gmail.com>
2024-03-25 12:56:31 +0900

    포트원 연동 테스트

---
+++ frontend/components/PortOne.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import * as PortOne from "@portone/browser-sdk/v2";
+
+  onMount(async () => {
+    const issueResponse = await PortOne.requestIssueBillingKey({
+      storeId: "store-bd7abf6b-fb2c-4f8e-b35c-93189fb5b5e7",
+      channelKey: "channel-key-2e5ffaf1-082b-476b-a8d3-3ef5ccdaa855",
+      billingKeyMethod: "CARD",
+      issueId: "120302130",
+      isTestChannel: true,
+      customer: {
+        customerId: "104903",
+        fullName: "원정후",
+        phoneNumber: "+8201075287237",
+        email: "cloe001000@gmail.com",
+      },
+    });
+    // 빌링키가 제대로 발급되지 않은 경우 에러 코드가 존재합니다
+    if (issueResponse.code != null) {
+      return alert(issueResponse.message);
+    }
+
+    console.log("빌링키:", issueResponse.billingKey);
+  });
+</script>
--- frontend/pages/index.ts
+++ frontend/pages/index.ts
@@ -5,6 +5,9 @@ import Share from "./share/Index.svelte";
 import Intro from "./intro/Index.svelte";
 import E404 from "./E404.svelte";
 
+// 결제 연동 창 개발하기 테스트용
+import PortOne from "../components/PortOne.svelte";
+
 interface Route {
   path: string;
   page: object;
@@ -16,5 +19,6 @@ export const routes: Route[] = [
   { path: "/account", page: Account },
   { path: "/console", page: Console },
   { path: "/share/:featureGroupId", page: Share },
+  { path: "/test", page: PortOne }, // 테스트용
   { path: "*", page: E404 }, // 주의: 가장 밑에 있어야 함
 ];
